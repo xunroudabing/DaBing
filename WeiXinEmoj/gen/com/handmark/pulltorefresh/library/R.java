6a0234e85967a5f4cc1f2a2ee2d86515d4e71cde
xunroudabing <hzroom110@163.com>
2013-10-25 16:54:52 +0800

    Signed-off-by: xunroudabing <hzroom110@163.com>

--- WeiXinEmoj/gen/com/handmark/pulltorefresh/library/R.java
+++ WeiXinEmoj/gen/com/handmark/pulltorefresh/library/R.java
@@ -9,19 +9,19 @@ package com.handmark.pulltorefresh.library;
 public final class R {
 	public static final class id {
 		public static final int pullFromStart = 0x7f090021;
-		public static final int pull_to_refresh_progress = 0x7f0900dd;
+		public static final int pull_to_refresh_progress = 0x7f0900e1;
 		public static final int rotate = 0x7f090027;
 		public static final int both = 0x7f090023;
 		public static final int webview = 0x7f09002a;
-		public static final int pull_to_refresh_text = 0x7f0900de;
+		public static final int pull_to_refresh_text = 0x7f0900e2;
 		public static final int pullDownFromTop = 0x7f090025;
 		public static final int gridview = 0x7f090029;
 		public static final int pullUpFromBottom = 0x7f090026;
 		public static final int scrollview = 0x7f09002b;
 		public static final int pullFromEnd = 0x7f090022;
-		public static final int pull_to_refresh_image = 0x7f0900dc;
-		public static final int fl_inner = 0x7f0900db;
-		public static final int pull_to_refresh_sub_text = 0x7f0900df;
+		public static final int pull_to_refresh_image = 0x7f0900e0;
+		public static final int fl_inner = 0x7f0900df;
+		public static final int pull_to_refresh_sub_text = 0x7f0900e3;
 		public static final int disabled = 0x7f090020;
 		public static final int flip = 0x7f090028;
 		public static final int manualOnly = 0x7f090024;
@@ -41,8 +41,8 @@ public final class R {
 		public static final int pull_to_refresh_from_bottom_release_label = 0x7f070043;
 	}
 	public static final class layout {
-		public static final int pull_to_refresh_header_horizontal = 0x7f030070;
-		public static final int pull_to_refresh_header_vertical = 0x7f030071;
+		public static final int pull_to_refresh_header_horizontal = 0x7f030071;
+		public static final int pull_to_refresh_header_vertical = 0x7f030072;
 	}
 	public static final class styleable {
 		public static final int PullToRefresh_ptrDrawableStart = 7;
@@ -67,11 +67,11 @@ public final class R {
 		public static final int PullToRefresh_ptrHeaderSubTextColor = 3;
 	}
 	public static final class drawable {
-		public static final int indicator_bg_top = 0x7f02010f;
-		public static final int default_ptr_flip = 0x7f02006a;
-		public static final int indicator_arrow = 0x7f02010d;
-		public static final int indicator_bg_bottom = 0x7f02010e;
-		public static final int default_ptr_rotate = 0x7f02006b;
+		public static final int indicator_bg_top = 0x7f020111;
+		public static final int default_ptr_flip = 0x7f02006c;
+		public static final int indicator_arrow = 0x7f02010f;
+		public static final int indicator_bg_bottom = 0x7f020110;
+		public static final int default_ptr_rotate = 0x7f02006d;
 	}
 	public static final class attr {
 		public static final int ptrHeaderBackground = 0x7f010068;
