85aad7899ca12657cbeba1aeddeff7b0f884e6ee
xunroudabing <hzroom110@163.com>
2013-12-06 14:11:05 +0800

    update QQ SDK

--- WeiXinEmoj/src/com/dabing/emoj/qqconnect/QQConnect.java
+++ WeiXinEmoj/src/com/dabing/emoj/qqconnect/QQConnect.java
@@ -1,9 +1,15 @@
 package com.dabing.emoj.qqconnect;
 
-import com.dabing.emoj.utils.AppConfig;
-import com.tencent.tauth.Tencent;
+import org.json.JSONObject;
 
+import com.dabing.emoj.utils.AppConfig;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
 /**
  * QQ互联
@@ -35,8 +41,8 @@ public class QQConnect {
 		long expire = AppConfig.getQQ_ExpiresIn(mContext);
 		long expire_in = (expire - System.currentTimeMillis())/1000;
 		if(access_token != null && openId != null && expire > 0){
-			mTencent.setAccessToken(access_token, String.valueOf(expire_in));
 			mTencent.setOpenId(openId);
+			mTencent.setAccessToken(access_token, String.valueOf(expire_in));
 		}
 		Log.d(TAG, "QQConnect.isSessionValid:"+mTencent.isSessionValid());
 	}
@@ -54,5 +60,36 @@ public class QQConnect {
 	}
 	public Tencent getTencent(){
 		return mTencent;
+	}
+	
+	public void shareToQQ(Activity activity,String filepath){
+		try {
+			Bundle bundle = new Bundle();
+			bundle.putInt(Tencent.SHARE_TO_QQ_KEY_TYPE, Tencent.SHARE_TO_QQ_TYPE_IMAGE);
+			bundle.putString(Tencent.SHARE_TO_QQ_IMAGE_LOCAL_URL, filepath);
+			mTencent.shareToQQ(activity, bundle, new IUiListener() {
+				
+				@Override
+				public void onError(UiError arg0) {
+					// TODO Auto-generated method stub
+					Log.d(TAG, "shareToQQ onError:"+arg0.errorMessage);
+				}
+				
+				@Override
+				public void onComplete(JSONObject arg0) {
+					// TODO Auto-generated method stub
+					Log.d(TAG, "shareToQQ onComplete:"+arg0);
+				}
+				
+				@Override
+				public void onCancel() {
+					// TODO Auto-generated method stub
+					Log.d(TAG, "onCancel");
+				}
+			});
+		} catch (Exception e) {
+			// TODO: handle exception
+			Log.e(TAG, e.toString());
+		}
 	}
 }
